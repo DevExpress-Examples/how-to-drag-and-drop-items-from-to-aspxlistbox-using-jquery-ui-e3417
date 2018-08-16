a1d217ea543815d74bd77e8f15a0972f68a67808
CORP\alakina.svetlana <svetlana.rebrova@devexpress.com>
2018-08-16 15:33:56 +0300

    18.1.3+
    18.1.3+

--- CS/WebSite/Default.aspx.cs
+++ CS/WebSite/Default.aspx.cs
@@ -1,2 +1,20 @@
-﻿public partial class _Default : System.Web.UI.Page {
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        lbAvailable.EnableItemsVirtualRendering = DevExpress.Utils.DefaultBoolean.False;
+        lbChosen.EnableItemsVirtualRendering = DevExpress.Utils.DefaultBoolean.False;
+    }
+
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
 }
